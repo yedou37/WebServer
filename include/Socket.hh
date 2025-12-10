edd5a1500d0ad12504b55cf3d1ef6983f5827cb2
yedou37 <yiedou1leo@163.com>
2025-12-10 17:50:19 +0800

    feat(net): implement Socket class with basic TCP socket operations

--- include/Socket.hh
+++ include/Socket.hh
@@ -1,7 +1,33 @@
 #pragma once
-#include <netinet/in.h>
-#include <sys/socket.h>
 
+#include "InetAddress.hh"
 #include "base/Macros.hh"
 
-class Socket {};
+class Socket {
+  using fd_t = int;
+
+public:
+  DISALLOW_COPY(Socket);
+
+  Socket();
+  explicit Socket(int fd);
+  ~Socket();
+
+  [[nodiscard]] int fd() const { return fd_; }
+
+  void bindAddress(const InetAddress& localaddr) const;
+  void listen() const;
+
+  int accept(InetAddress* peeraddr) const;
+
+  void setReuseAddr(bool on) const;
+
+  void setReusePort(bool on) const;
+
+  void setKeepAlive(bool on) const;
+
+  void setTcpNoDelay(bool on) const;
+
+private:
+  const fd_t fd_;
+};
--- src/net/Socket.cpp
+++ src/net/Socket.cpp
@@ -1,1 +1,88 @@
 #include "Socket.hh"
+
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <stdexcept>
+
+#include "InetAddress.hh"
+
+Socket::Socket() : fd_(::socket(AF_INET, SOCK_STREAM, 0)) {
+  if (fd_ == -1) {
+    perror("Socket::Socket(): socket() failed");
+    throw std::runtime_error("Socket::Socket(): socket() failed");
+  }
+}
+
+Socket::Socket(int fd) : fd_(fd) {
+  if (fd_ < 0) {
+    perror("Socket create error");
+    throw std::runtime_error("Socket create error");
+  }
+}
+
+Socket::~Socket() {
+  if (fd_ != -1) {
+    ::close(fd_);
+  }
+}
+
+void Socket::bindAddress(const InetAddress& localaddr) const {
+  auto ret = ::bind(fd_, localaddr.GetSocketAddr(), localaddr.GetSocketAddrLen());
+  if (ret != 0) {
+    perror("Socket::bindAddress error");
+    throw std::runtime_error("Socket::bindAddress error");
+  }
+}
+
+void Socket::listen() const {
+  auto ret = ::listen(fd_, SOMAXCONN);
+  if (ret != 0) {
+    perror("Socket::listen error");
+    throw std::runtime_error("Socket::listen error");
+  }
+}
+
+int Socket::accept(InetAddress* peeraddr) const {
+  InetAddress::sin_t addr;
+  socklen_t len = sizeof(addr);
+  memset(&addr, 0, sizeof(addr));
+
+  int connfd = ::accept(fd_, reinterpret_cast<sockaddr*>(&addr), &len);
+
+  if (connfd >= 0) {
+    // 如果成功，利用 InetAddress 的构造函数，把 sock_in 转换成对象
+    // 这里利用了赋值操作符，将 peeraddr 指向的内容更新
+    if (peeraddr != nullptr) {
+      *peeraddr = InetAddress(addr);
+    }
+  } else {
+    // TODO(yedou): 在非阻塞模式下，需要处理 EAGAIN 和 EWOULDBLOCK，不能视为错误
+    perror("Socket::accept error");
+  }
+
+  return connfd;
+}
+
+void Socket::setReuseAddr(bool on) const {
+  int optval = on ? 1 : 0;
+  ::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+}
+
+void Socket::setReusePort(bool on) const {
+  int optval = on ? 1 : 0;
+  ::setsockopt(fd_, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+}
+
+void Socket::setKeepAlive(bool on) const {
+  int optval = on ? 1 : 0;
+  ::setsockopt(fd_, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval));
+}
+
+void Socket::setTcpNoDelay(bool on) const {
+  int optval = on ? 1 : 0;
+  // 禁用 Nagle 算法
+  ::setsockopt(fd_, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval));
+}
